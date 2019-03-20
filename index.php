702b1746359d5d04935117a2cfef1719e47bcfa6
lvtan13 <levantan@tdc.edu.vn>
2019-03-20 19:19:37 +0700

    qlph

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Phòng Học TDC</title>
+  <title>Phòng Học - Trường Cao Đẳng Công Nghệ Thủ Đức</title>
   
   <link rel="stylesheet" type="text/css" href="view/bootstrap/css/w3.css">
   <link rel="stylesheet" type="text/css" href="view/bootstrap/css/style.css">
@@ -68,7 +68,9 @@
 </div>
 <!-- Menu center-->
 <div class="container-fluid">
- 
+     <?php
+      require("view/menutest.php");
+    ?>
 </div>
 <!-- End Menu center-->
 
@@ -86,14 +88,9 @@
   </div>
   <!-- End Footer -->
 
-  
   <script src="view/bootstrap/js/jquery.min.js"></script>
+   <script src="view/bootstrap/js/bootstrap.bundle.min"></script>
   <script src="view/bootstrap/js/bootstrap.min.js"></script>
   <script src="view/bootstrap/js/script.js"></script>
-  
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
+ </body>
 </html>
