16c551e918c613abaa5028d6f445f50e517f71ac
lvtan13 <levantan@tdc.edu.vn>
2019-03-22 16:12:51 +0700

    220319

--- View/form_phanhoi.php
+++ View/form_phanhoi.php
@@ -1,12 +1,37 @@
-<div class="container-fluid">
-    <div class="float-left">
-        Phản hồi mới nhất
-    </div>
-    <div>
-                <textarea style="width: 100%"placeholder="vui lòng nhập ghi nhận"></textarea>
-                      
-    </div>
-    <div class="float-right">
-    	 <input class="bg-secondary font-weight-bold" type="submit" value="Gửi">
-    </div>
-</div>
+ <!-- Blog Comments -->
+                <!-- Comments Form -->
+                <div>
+                    <h4 class="text-left">Viết phản hồi <?=$chitietph->ten_phonghoc?><span class="fa fa-glyphicon-pencil"></span></h4>
+                    <form role="form">
+                        <div class="form-group">
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                        <div class="float-right"><button type="submit" class="btn btn-primary">Gửi</button> || <input class="btn btn-primary" type="reset"></div>
+                    </form>
+  				</div>
+                <!-- Posted Comments -->
+                <!-- Comment -->
+                <div class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+                <!-- Comment -->
+                <div class="media text-left">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading">Start Bootstrap
+                            <small>August 25, 2014 at 9:30 PM</small>
+                        </h4>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                </div>
+               
--- chitiet.php
+++ chitiet.php
@@ -1,86 +1,65 @@
 <?php
       include("Controller/C_phonghoc.php");
       $c_phong_hoc=new C_phonghoc();
-      $noi_dung=$c_phong_hoc->index();
-      $slide=$noi_dung['slide'];
-      $menu=$noi_dung['menu'];
-      $menukhuvuc=$noi_dung['menukhuvuc'];
-      $loaiph=$noi_dung['loaiph'];
-      $phonghoc=$noi_dung['phonghoc'];
-      //print_r($phonghoc);
+      $noi_dung=$c_phong_hoc->chitietphonghoc();
+      $chitietph=$noi_dung['chitietph'];
+
+      $noidung=$c_phong_hoc->index();
+      $slide=$noidung['slide'];
+      $menu=$noidung['menu'];
+      $menukhuvuc=$noidung['menukhuvuc'];
+      //print_r($menu);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Phòng Học - Trường Cao Đẳng Công Nghệ Thủ Đức</title>
-  
+  <title>Phòng Học TDC</title>
   <link rel="stylesheet" type="text/css" href="view/bootstrap/css/w3.css">
   <link rel="stylesheet" type="text/css" href="view/bootstrap/css/style.css">
   <link rel="stylesheet" type="text/css" href="view/bootstrap/font/css/all.min.css">
   <link rel="icon" href="images/logo.png" type="image/x-icon">
   <link rel="stylesheet" href="view/bootstrap/css/bootstrap.min.css">
-  
 </head>
 <body>
-  <!--Baner top-->
-<div class="container-fluid paddingall">
-      <?php
-        require("view/banertop.php");
-      ?>
-</div>
-
-  <!--End Baner top -->
-
-  <!-- Menu -->
-<div class="container-fluid sticky-top">
-      <?php
-        require("view/menu_top.php");
-      ?>
-  </div>
-  <!-- End Menu -->
-
-<div class="container-fluid content">
-<div class="row">
-  <!--Left-->
-  <div class="col-lg-9 border border-primary">
-    <div class="container-fluid"><img src="http://placehold.it/900x300" alt=""></div>
-  </div> 
-  <!--End Left-->
-
-  <!-- Slide-->
-  <div class="col-lg-3 border border-primary">
-  <div class="card">
-      <div class="card-head border border-primary">
-        <img src="http://placehold.it/40x40" alt="">
-        <h5>Thông báo 1</h5>
-      </div>
-      <div class="card-body">
-        <a href="#">Thông tin phản hồi phòng học ?</a>
-      </div>
-  </div>
-  </div>
-  <!-- End Slide -->
-</div>
-<!-- Menu center-->
-<div class="container-fluid">
-
-</div>
-<!-- End Menu center-->
-
- <!-- Footer -->
-  <div class="container-fluid">
-    <?php
-      require("view/Footer.php");
-    ?>
-  </div>
-  <!-- End Footer -->
-
-  <script src="view/bootstrap/js/jquery.min.js"></script>
-   <script src="view/bootstrap/js/bootstrap.bundle.min"></script>
-  <script src="view/bootstrap/js/bootstrap.min.js"></script>
-  <script src="view/bootstrap/js/script.js"></script>
-  <script src="view/bootstrap/js/popper.min.js"></script>
- </body>
+	<!-- Baner top -->
+	<div class="container-fluid paddingall">
+		<?php require("view/banertop.php"); ?>
+	</div>
+	<!-- Menu top -->
+		<div class="container-fluid sticky-top">
+		<?php require("view/menu_top.php"); ?>
+	</div>
+	<!-- content -->
+	<div class="container-fluid paddingall">
+			<div class="row">
+				<!-- Content left -->
+			<div class="col-xl-8 col-md-8 col-sm-6 mb-4">
+				<div>
+					<!-- Preview Image -->
+               	    <img class="d-block w-100 h-25" src="images/lasan1.jpg" alt="">
+       				<h1 class="text-left">Thông Tin <?=$chitietph->ten_phonghoc?></h1>
+					<h4 class="text-left"><?=$chitietph->mo_ta_ngan_ph?></h4>
+					<p class="text-justify"><?=$chitietph->mo_ta_chi_tiet_ph?></p>
+				</div>
+				<div>
+					<?php require("view/form_phanhoi.php");?>
+				</div>
+			</div>
+			<!-- content right -->
+			<div class="col-xl-4 col-md-4">
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div><?php require("view/footer.php");?></div>
+		<div><a class="text-white font-weight-bold" href="index.php">Quay lại trang chủ</a></div>
+	</div>
+<script src="view/bootstrap/js/jquery.min.js"></script>
+<script src="view/bootstrap/js/bootstrap.bundle.min"></script>
+<script src="view/bootstrap/js/bootstrap.min.js"></script>
+<script src="view/bootstrap/js/script.js"></script>
+<script src="view/bootstrap/js/popper.min.js"></script>
+</body>
 </html>
